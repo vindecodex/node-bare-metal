d31adb9016313781ad65452a7415bf6f0ff8782e
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-07 17:18:00 +0800

    added route for person?id

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const replaceTemplate = require('./modules/replaceTemplate');
 const main = fs.readFileSync('./templates/main.html', 'utf-8');
 const fourofour = fs.readFileSync('./templates/404.html', 'utf-8');
 const personCards = fs.readFileSync('./templates/person-cards.html', 'utf-8');
+const person = fs.readFileSync('./templates/person.html', 'utf-8');
 
 
 // DATA
@@ -19,6 +20,7 @@ const server = http.createServer(function(req,res){
   // we use url module to separate query params and pathname
   // try to console.log url.parse(req.url, true); to see what is contains
   const {pathname, query} = url.parse(req.url, true);
+  console.log(pathname, query)
   if (pathname === '/') {
     res.writeHead(200, {
       'Content-type': 'text/html'
@@ -29,6 +31,14 @@ const server = http.createServer(function(req,res){
     let mainHTML = main.replace(/{%PERSONCARDS%}/g,setCards);
     // rendering our html file
     res.end(mainHTML);
+  } else if (pathname === '/person') {
+    // we pass the id of query url to get the specific object element
+    const personData = dataObj.find(function(person){
+      return person.id === Number(query.id);
+    })
+    // we again use the replaceTemplate that we create to replace template strings on our html
+    const output = replaceTemplate(person,personData);
+    res.end(output);
   } else {
     res.writeHead(404);
     // rendering our html file
